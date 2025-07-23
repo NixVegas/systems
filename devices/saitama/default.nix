dbd5733b0ca1deffe3fa2fd60e9dd75fee0933a1
Tristan Ross <tristan.ross@midstall.com>
2025-07-22 20:44:40 -0700

    Configure eth ports on Saitama

--- devices/saitama/default.nix
+++ devices/saitama/default.nix
@@ -12,11 +12,19 @@
     hostId = "6e2e597d";
   };
 
-  services.hydra = {
-    enable = true;
-    hydraURL = "http://saitama.local";
-    port = 80;
-    notificationSender = "nobody@saitama.local";
+  services = {
+    hydra = {
+      enable = true;
+      hydraURL = "http://saitama.local";
+      port = 80;
+      notificationSender = "nobody@saitama.local";
+    };
+    openssh.openFirewall = false;
+  };
+
+  networking.firewall.interfaces.enP3p3s0f0 = {
+    allowedTCPPorts = [ 22 80 ];
+    allowedUDPPorts = [ 22 80 ];
   };
 
   nixpkgs.system = "aarch64-linux";
