4cab3986346a6574bbd27b936312961f7bc79a34
Tristan Ross <tristan.ross@midstall.com>
2025-07-26 22:28:42 -0700

    Set up networking for tatsumaki

--- devices/tatsumaki/default.nix
+++ devices/tatsumaki/default.nix
@@ -11,9 +11,53 @@
     "usbhid"
   ];
 
+  services.openssh.openFirewall = false;
+
   networking = {
+    useDHCP = false;
     hostName = "tatsumaki";
     hostId = "7144d96e";
+    vlans = {
+      "trunk1.build" = {
+        id = 2;
+        interface = "enP3p3s0f0";
+      };
+      "trunk2.build" = {
+        id = 2;
+        interface = "enP3p3s0f1";
+      };
+    };
+    interfaces = {
+      build.useDHCP = true;
+      noc.useDHCP = true;
+      usb0.useDHCP = true;
+    };
+    bridges = {
+      build.interfaces = [ "trunk1.build" "trunk2.build" ];
+      noc.interfaces = [ "enP3p5s0" ];
+    };
+    firewall.interfaces = {
+      build = {
+        allowedTCPPorts = [
+          22
+          80
+        ];
+        allowedUDPPorts = [
+          22
+          80
+        ];
+      };
+      noc = {
+        allowedTCPPorts = [
+          22
+          80
+        ];
+        allowedUDPPorts = [
+          22
+          80
+        ];
+      };
+    };
   };
 
   nixpkgs.system = "aarch64-linux";
