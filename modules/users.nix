7dc27d790089f04e8dbba64eab93bbfc0ce410e3
Morgan Jones <me@numin.it>
2025-07-27 20:04:25 -0700

    numinit: add authorized key

--- modules/users.nix
+++ modules/users.nix
@@ -28,6 +28,9 @@
       numinit = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+          "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBOLkms0KUv8J45FqK2WG6J6X4DZGhMB5sMM8gEl0bUCmH7XH36/D73+nDtVriXC2ITAduvKmCRvs+DW1js3jTwQAAAAEc3NoOg== numinit@cyrus#6460026"
+        ];
       };
     };
   };
