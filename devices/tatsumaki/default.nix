535249141b4a15b79df614b940510cf32d27a46d
Morgan Jones <me@numin.it>
2025-07-30 05:12:25 +0000

    tatsumaki: deprioritize noc over build

--- devices/tatsumaki/default.nix
+++ devices/tatsumaki/default.nix
@@ -27,6 +27,15 @@
       noc.useDHCP = true;
       usb0.useDHCP = true;
     };
+    dhcpcd.extraConfig = ''
+      # deprioritize noc over build
+      interface build
+      metric 1000
+      interface noc
+      metric 1001
+      interface usb0
+      metric 2000
+    '';
     bridges = {
       build.interfaces = [ "enP3p3s0f0" "trunk2.build" ];
       noc.interfaces = [ "enP3p5s0" ];
