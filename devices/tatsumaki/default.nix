a99bb272c38ad17200c288fd2675357a92b2dac6
Morgan Jones <me@numin.it>
2025-07-30 05:30:24 +0000

    tatsumaki: comment

--- devices/tatsumaki/default.nix
+++ devices/tatsumaki/default.nix
@@ -37,6 +37,7 @@
       metric 2000
     '';
     bridges = {
+      # let us plug into the right 10g for a direct link into the buildnet
       build.interfaces = [ "enP3p3s0f0" "trunk2.build" ];
       noc.interfaces = [ "enP3p5s0" ];
     };
