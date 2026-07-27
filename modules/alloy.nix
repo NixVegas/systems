a22cec670d4f22f51a9105fb6816c8d0d85b64b1
Robert James Hernandez <rob@sarcasticadmin.com>
2026-07-27 04:05:30 +0000

    modules.alloy: init prom nixos-exporter

--- modules/alloy.nix
+++ modules/alloy.nix
@@ -141,5 +141,19 @@ in
         configPath = alloyConfig;
       };
     }
+
+    {
+      systemd.services.prometheus-nixos-exporter = {
+         wantedBy = [ "multi-user.target" ];
+         after = [ "network.target" ];
+         path= [ pkgs.nix pkgs.bash ];
+         serviceConfig = {
+           Restart = "always";
+           RestartSec = "60s";
+           ExecStart = "${pkgs.prometheus-nixos-exporter}/bin/prometheus-nixos-exporter";
+         };
+      };
+    }
+
   ];
 }
